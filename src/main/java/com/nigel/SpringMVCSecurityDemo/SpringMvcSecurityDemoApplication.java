944fc1792be1152264ef2a0786433c8e76ff88ac
Nigel <mail@nigelmoes.nl>
2024-04-02 08:37:29 -0500

    Initial commit

---
+++ src/main/java/com/nigel/SpringMVCSecurityDemo/SpringMvcSecurityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nigel.SpringMVCSecurityDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSecurityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcSecurityDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nigel/SpringMVCSecurityDemo/SpringMvcSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nigel.SpringMVCSecurityDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
